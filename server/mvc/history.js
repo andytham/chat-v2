2cb1a7be77ea22d2e4fd824022722f7433568f2d
andy <thamandyy@gmail.com>
2019-01-04 17:15:29 -0500

    History MVC

---
+++ server/mvc/history.js
@@ -0,0 +1,62 @@
+const History = require('../models/history');
+
+const historyController = {};
+const db = require('../db/config');
+const History = {};
+
+History.findAll = () => {
+	return db.query(
+		`
+		SELECT * FROM chat_history
+		`
+	);
+};
+
+History.create = history => {
+	return db.one(
+		`
+		INSERT INTO chat_history
+		(usr, msg, tme)
+		VALUES ($1, $2, $3)
+		RETURNING *
+		`,
+		[history.usr, history.msg, history.tme]
+	)
+}
+
+historyController.index = (req, res) => {
+	History.findAll()
+	.then(history => {
+		res.json(history);
+	})
+	.catch(err => {
+		console.log(err);
+		res.status(500).json({err})
+	})
+}
+
+historyController.create = (req, res) => {
+	console.log(req.body);
+	History.create({
+		usr: req.body.usr,
+		msg: req.body.msg,
+		tme: req.body.tme
+	})
+	.then(history => {
+		res.json({
+			message: "history entry created",
+			data: history
+		})
+	})
+	.catch(err => {
+		console.log(err);
+	})
+}
+
+const express = require('express');
+const historyRouter = express.Router();
+
+historyRouter.get('/', historyController.index);
+historyRouter.post('/', historyController.create);
+
+module.exports = historyRouter;
