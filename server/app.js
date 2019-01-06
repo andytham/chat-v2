44df7c7f44a851405acf78b912b440f3f65b8119
andy <thamandyy@gmail.com>
2019-01-06 16:46:40 -0500

    Socket.io emitters and time function

--- server/app.js
+++ server/app.js
@@ -3,8 +3,14 @@ const app = express();
 const path = require('path');
 
 const PORT = process.env.PORT || 8080;
-const server = require('http').createServer(app);
-
+const timeGet = function () {
+	let t = new Date;
+	let tM = t.getMinutes();
+	if (t.getMinutes() < 10){
+		tM = "0" + t.getMinutes();
+	}
+	return `${t.getHours()}:${tM}`;
+}
 // CORS
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -13,6 +19,29 @@ app.use(function(req, res, next) {
   next();
 });
 
+//socket.io
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const cr = require('./chat-socket.js');
+let Chatroom = cr();
+let usersList = {};
+//server side socket emitters
+io.on('connection', function(socket){
+	  //when user joins the server
+		socket.on('join', function(user){
+			io.emit('history', Chatroom.getChatHistory())
+			if(user){
+				console.log(user, "has joined");
+
+				Chatroom.addEntry({usr: "server", msg: `${user} has joined the server`, tme: timeGet()})
+				io.emit('message', Chatroom.getChatHistory())
+				usersList[socket.id] = user
+			}
+		})
+})
+
+
+//URL get
 app.use(express.static('build'));
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname + '../index.html'))
@@ -28,10 +57,6 @@ const historyRoutes = require('./mvc/history.js');
 app.use('/api/history', historyRoutes);
 
 server.listen(PORT, (err) => {
-	let t = new Date;
-	let tM = t.getMinutes();
-	if (t.getMinutes() < 10){
-		tM = "0" + t.getMinutes();
-	}
-	console.log(`Listening on port ${PORT}, ${t.getHours()}:${tM}`);
+
+	console.log(`Listening on port ${PORT}, ${timeGet()}`);
 })
---
+++ server/chat-socket.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+
+module.exports = function () {
+  let chatHistory = [{usr: 'server', msg: 'welcome to the chatroom!', tme: ''}] //fallback
+  axios.get(`http://8080/api/history`).then( data => {
+    chatHistory = data.data;
+  }).catch(err => {
+    console.log('most likely no server found');
+  })
+
+  // function broadcastMessage(message) {
+  //   members.forEach(m => m.emit('message', message))
+  // }
+
+  function addEntry(entry) {
+    chatHistory = chatHistory.concat(entry)
+    console.log('this is entry', entry);
+    axios.post(`http://8080/api/history`,
+    {
+      usr: entry.usr,
+      msg: entry.msg,
+      tme: entry.tme
+    })
+    .then(res => {
+      // console.log(res);
+      console.log("post successful");
+    })
+    .catch(err => {
+      console.log("post failed");
+      // console.log(err);
+    })
+    // console.log("added to chat history: ", chatHistory);
+  }
+
+  function getChatHistory() {
+    // console.log("getting chat history: ", chatHistory);
+    return chatHistory.slice()
+  }
+
+
+  return {
+    // broadcastMessage,
+    addEntry,
+    getChatHistory,
+  }
+}
