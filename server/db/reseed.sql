8e6b0ac04f76105c62a08a2e04d3f2df5e682ed4
andy <thamandyy@gmail.com>
2019-02-08 21:09:06 -0500

    UserSessions DB

--- server/db/reseed.sql
+++ server/db/reseed.sql
@@ -3,3 +3,6 @@ DROP TABLE  IF EXISTS  chat_history CASCADE;
 \! pwd
 \i 'server/db/migrations/history-migration.sql'
 \i 'server/db/seeds/history-seed.sql'
+
+DROP TABLE IF EXISTS users_sessions CASCADE;
+\i 'server/db/seeds/session-migration'
