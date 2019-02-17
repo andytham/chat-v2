f1832aa805cfcbacafaf6f6effd7ea896b39357e
andy <thamandyy@gmail.com>
2019-02-17 00:43:57 -0500

    More fallback cases

--- server/game/main.js
+++ server/game/main.js
@@ -5,3 +5,40 @@ let movement = {
 	left: false,
 	right: false
 }
+
+document.addEventListener('keydown', function(event) {
+  switch (event.keyCode) {
+		case 65: // a
+		case 37: // arrow 
+      movement.left = true;
+      break;
+		case 87: // w
+		case 38: // arrow
+		case 32: // space
+      movement.up = true;
+      break;
+		case 68: // d
+		case 39: // arrow
+      movement.right = true;
+      break;
+
+  }
+});
+
+document.addEventListener('keyup', function(event) {
+  switch (event.keyCode) {
+		case 65: // a
+		case 37: // arrow 
+      movement.left = false;
+      break;
+		case 87: // w
+		case 38: // arrow
+		case 32: // space
+      movement.up = false;
+      break;
+		case 68: // d
+		case 39: // arrow
+      movement.right = false;
+      break;
+  }
+});
