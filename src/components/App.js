3f8cba880269da66a1a2de4ddb6510ceab8c0786
andy <thamandyy@gmail.com>
2019-03-04 22:14:21 -0500

    Cleaned code

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Route, Redirect, withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { Route } from 'react-router-dom';
+
 //components
 import { Home } from './Home';
 import { Chatroom } from './Chatroom';
 import { Login } from './Login';
-import { Logout } from './Logout';
 import { Register } from './Register';
+
 //css
 import '../css/reset.scss'
 import '../css/App.scss'
@@ -14,16 +14,15 @@ import '../css/Form.scss'
 import '../css/Chatroom.scss'
 import '../css/UsersList.scss'
 import '../css/Night.scss'
-class App extends React.Component {
 
+class App extends React.Component {
 	render(){
 		return(
 			<div id="auth" className="App">
-				<Route exact path="/" render={(props) => <Home />} />
-				<Route exact path="/login" render={(props) => <Login />} />
-				<Route exact path="/register" render={(props) => <Register />} />
-				<Route exact path="/chat" render={(props) => <Chatroom /> } />
-				{/* <Logout /> */}
+				<Route exact path="/" render={() => <Home />} />
+				<Route exact path="/login" render={() => <Login />} />
+				<Route exact path="/register" render={() => <Register />} />
+				<Route exact path="/chat" render={() => <Chatroom /> } />
 			</div>
 		)
 	}
